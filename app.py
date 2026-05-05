d81fa388a412f0c51692f332888e73ff6f01e81c
rejupillai <reju.pillai@gmail.com>
2026-05-04 23:02:55 -0700

    Added Apache 2.0 license

--- app.py
+++ app.py
@@ -1,3 +1,18 @@
+#  Copyright 2026 Google LLC
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+
 import streamlit as st
 import streamlit.components.v1 as components
 from google import genai
@@ -7,7 +22,7 @@ from google.genai import types
 client = genai.Client()
 
 st.set_page_config(page_title="Gemini Grounding & Citations", layout="centered")
-st.title("🔍 Gemini Grounding with Citations")
+st.title("Gemini Grounding with Citations")
 
 def add_inline_citations(response):
     """
